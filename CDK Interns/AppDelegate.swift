7f702c712663b3e4f6e9a7ff3090055be3f50dae
Ong, Matthew <Matthew.Ong@cdk.com>
2015-08-06 14:21:15 -0700

    wip push note respond

--- CDK Interns/AppDelegate.swift
+++ CDK Interns/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var dataManager = DataManager()
     
+    var dUserInfo: [NSObject : AnyObject]?
+
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
        
         /*
@@ -54,6 +56,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UIApplication.sharedApplication().registerUserNotificationSettings(types)
         UIApplication.sharedApplication().registerForRemoteNotifications()
         
+        if let info = launchOptions?[UIApplicationLaunchOptionsRemoteNotificationKey] as? [NSObject : AnyObject] {
+            dUserInfo = info
+        }
+        
 //        let notificationTypes:UIUserNotificationType = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
 //        let notificationSettings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: notificationTypes, categories: nil)
 //        
@@ -66,29 +72,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     }
     
-//    func application(application: UIApplication!, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings!) {
-//        UIApplication.sharedApplication().registerForRemoteNotifications()
-//    }
-//    
-//    func application(application: UIApplication!, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData!) {
-//        let currentInstallation: PFInstallation = PFInstallation.currentInstallation()
-//        currentInstallation.setDeviceTokenFromData(deviceToken)
-//        currentInstallation.saveInBackground()
-//        let foo =
-//    }
-//    
-//    func application(application: UIApplication!, didFailToRegisterForRemoteNotificationsWithError error: NSError!) {
-//        println(error.localizedDescription)
-//    }
-    
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
-        println("Got token data! \(deviceToken)")
+        NSLog("Got token data! \(deviceToken)")
     }
     
     func application(application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-        println("Couldn't register: \(error)")
+        NSLog("Couldn't register: \(error)")
     }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject: AnyObject]) {
+        dUserInfo = userInfo
 
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        self.yourAction(dUserInfo)
+    }
+    
+    //MAKE THIS CUSTOM FUNCTION
+    func yourAction(userInfo: [NSObject : AnyObject]?) {
+        if let info = userInfo?["aps"] as? Dictionary<String, AnyObject> {
+        }
+    }
+    
+    
+    
+    
+    
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -101,10 +113,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(application: UIApplication) {
